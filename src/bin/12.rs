cbce5f664f5dc4f34ff2aa06b9e1d899b0965712
tomkimsour <tomkimsour@gmail.com>
2022-12-15 17:46:00 +1030

    almost there

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -2,41 +2,74 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
-use std::{str::Lines, collections::{HashMap, HashSet}, hash::{Hash, Hasher}};
+use std::{str::Lines, collections::{HashMap, HashSet, BinaryHeap}, hash::{Hash, Hasher}};
 
 use itertools::Itertools;
 use nalgebra::{Point2};
-
+#[derive(Copy,Clone,Debug,PartialEq,Eq,Hash)]
 struct Node {
     value : u8,
     visited : bool,
-    neighbours : HashMap<u32,Vec<Node>>,
+    cost: u32,
 }
 
 impl Node {
     fn new() -> Node{
-        Node  {value: (0), visited: (false), neighbours: (HashMap::new())}
+        Node  {value: (0), visited: (false), cost: (0)}
     }
 
     fn from(value:u8) -> Node{
-        Node { value: (value), visited: (false), neighbours: (HashMap::new())}
-    }
-
-    fn add_neighbour(&self,row:usize, col:usize, node_matrix:Vec<Vec<Node>>){
-        // check top
-        // check left
-        // check right 
-        // check bottom 
+        Node { value: (value), visited: (false), cost: (0)}
     }
 }
 
+
+#[derive(Debug)]
 struct HeightMap {
-    map : HashMap<u32,Vec<Node>>,
-    start : Point2<u32>,
-    end : Point2<u32>,
+    adjacency_list: HashMap<u32,Vec<Node>>,
+    start : Node,
+    end : Node,
 }
 
 impl HeightMap {
+
+    fn add_neighbour(row:usize, col:usize, node_matrix:&Vec<Vec<Node>>) -> Vec<Node>{
+        let mut neighbours: Vec<Node> = Vec::new();
+        // check top
+        if row > 0{
+            let top_diff = node_matrix[row][col].value as i8 - node_matrix[row-1][col].value as i8;
+            if  top_diff.abs() <= 1 {
+                neighbours.push(node_matrix[row-1][col].clone());
+            }
+        }
+
+        // check left
+        if col > 0 {
+            let left_diff = node_matrix[row][col].value as i8 - node_matrix[row][col-1].value as i8;
+            if left_diff.abs() <= 1 {
+                neighbours.push(node_matrix[row][col-1].clone());
+            }
+        }
+
+        // check right 
+        if col < node_matrix[0].len() -1 {
+            let right_diff = node_matrix[row][col].value as i8 - node_matrix[row][col+1].value as i8;
+            if right_diff.abs() <= 1 {
+                neighbours.push(node_matrix[row][col+1].clone());
+            }
+        }
+
+        // check bottom 
+        if row < node_matrix.len() -1 {
+            let bottom_diff = node_matrix[row][col].value as i8 - node_matrix[row+1][col].value as i8;
+            if bottom_diff.abs() <= 1 {
+                neighbours.push(node_matrix[row+1][col].clone());
+            }
+        }
+
+        neighbours
+    }
+
     fn new(lines: Lines) -> HeightMap{
 
         // create a matrix of nodes
@@ -61,37 +94,57 @@ impl HeightMap {
         }
 
         // fill node neighbours and create set of unvisited node
-        let mut pending = HashSet::new();
-        for row in 0..node_matrix.len() {
-            for col in 0..node_matrix[0].len() {
-                node_matrix[row][col].add_neighbour(row,col,node_matrix);
-                pending.insert(&node_matrix[row][col]);
+        let mut adjacency_list: HashMap<u32,Vec<Node>> = HashMap::new();
+        let row_len = node_matrix.len();
+        let col_len= node_matrix[0].len();
+        for row in 0..row_len {
+            for col in 0..col_len {
+                let adjacent_nodes =  HeightMap::add_neighbour(row,col,&node_matrix);
+                adjacency_list.insert(row as u32 * node_matrix[0].len() as u32 + col as u32 +1, adjacent_nodes);
             }
         }
 
-        let height_map: HeightMap = HeightMap { map: (vec), start: (start), end: (end) };
+        let height_map: HeightMap = HeightMap { adjacency_list: (adjacency_list), start: (start), end: (end) };
         height_map
     }
 }
 
-fn dijkstra()-> u32{
+fn dijkstra(adjacency_list: HashMap<u32,Vec<Node>>, start: Node, end: Node)-> u32{
     // Create a set of node that are unvisited
+    let mut unvisited  = HashSet::new();
+    for (key, value) in adjacency_list.iter() {
+        for node in value {
+            unvisited.insert((key,node.clone()));
+        }
+    }
 
-    // 
+    // create priority queue out of the adjacency list
+    let mut dist: Vec<_> = (0..unvisited.len()).map(|_| usize::MAX).collect();
+    dist[start.value as usize] = 0;
+
+    let mut heap = BinaryHeap::from(dist);
+    // while unvisited is not empty
+    for i in 0..unvisited.len() {
+        let v = heap.pop().unwrap();
+        // for each neighbour of v
+        for neighbour in adjacency_list.get(&(v as u32)).unwrap(){
+            // if not visited and distance is smaller than the current value
+            if !neighbour.visited && neighbour.value as usize < v.value + 1 {
+
+            }
+        }
+    }
+    
     0
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let lines = input.lines();
     let height_map = HeightMap::new(lines);
-    for row in height_map.map {
-        for col in row {
-            print!("{} ",col);
-        }
-        println!();
-    }
-    println!("{}",height_map.start);
-    println!("{}",height_map.end);
+    println!("{:?}",height_map.adjacency_list);
+    println!("{:?}",height_map.start);
+    println!("{:?}",height_map.end);
+    let min_path = dijkstra(height_map.adjacency_list, height_map.start, height_map.end);
     
     Some(0)
 }
